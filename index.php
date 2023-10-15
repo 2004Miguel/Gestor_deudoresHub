da6d45103a960d8cf044755272d643cbe3d40768
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-10-15 17:04:19 -0500

    Se logró hacer el titulo y el form; instalar con exito el servidor (xampp) y se descubrió que hay que repasar html y css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar deudores</title>
+</head>
+<body>
+    <header>
+        <h1>Bien venido</h1>
+        <p>Busque información del deudor ingresando el nombre</p>
+    </header>
+
+    <main>
+        <form acition="resultado.php" method="post">
+            <input type="text"></input>
+            <button  type="submit" name="btnEnviar" id="btnEnviar"><a href="resultado.php" target="_blank"></a>Click para buscar</button>
+        </form>
+    </main>
+    
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,9 @@
+<?php
+
+    if (isset($__POST['btnEnviar'])) {
+    E    cho "Se ha pulsado el botón aceptar";
+    } else {
+        Echo "Se ha pulsado el botón cancelar";
+    }
+
+?>
