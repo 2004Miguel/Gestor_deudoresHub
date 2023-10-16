2d02ce2d456fee5422aa6e18a4b37bf315f78b6a
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-10-16 11:55:20 -0500

    Se cambió el label

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
         <h1>Bien venido</h1>
     </header>
     <form action="">
-        <label for="nombre_deudor"></label>
+        <label for="nombre_deudor">
         <span>Nombre del deudor</span>
         <input name="nombre_deudor" id="nombre_deudor" type="text">
+        </label>
         <input type="submit">
     </form>
-    
 </body>
-</html>
+</html> 
